22b98d7bdcece9918f7e94c4ab41c823d3d71824
MopuK <stanislavgusev@gmail.com>
2026-05-25 21:15:18 +0300

    Added cors

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv()
 app = Flask(__name__, static_folder="../static")
 db_uri = os.environ.get("DATABASE_URI")
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+CORS(app, supports_credentials=True, origins=os.environ.get("FRONTEND_URL"))
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
