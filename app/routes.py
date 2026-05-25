de9dcb6b5854b5a615df694bcfc233c09ab0e0c5
MopuK <stanislavgusev@gmail.com>
2026-05-25 21:37:28 +0300

    Fixed catalog

--- app/routes.py
+++ app/routes.py
@@ -7,59 +7,66 @@ from sqlalchemy.orm import selectinload
 @app.get("/api/products")
 def getProducts():
     
-    products = db.session.scalars(select(models.Product)
-                                  .options(selectinload(models.Product.variants)
-                                           .selectinload(models.ProductVariant.images))
-                                  ).all()
+    variants = db.session.scalars(select(models.ProductVariant)
+    .options(
+        selectinload(models.ProductVariant.product),
+        selectinload(models.ProductVariant.images),
+        selectinload(models.ProductVariant.size),
+        selectinload(models.ProductVariant.color),
+        selectinload(models.ProductVariant.material),
+    )).all()
     
     return {
-        "products": [
-            {
-                "id": p.id,
-                "name": p.name,
-                "variants": [
-                    {
-                        "id": v.id,
-                        "price": v.variant_price,
-                        "stock": v.stock,
-                        "is_available": v.is_available,
-                        "color": {
-                            "name": v.color.name,
-                            "hex_code": v.color.hex_code,
-                            "slug": v.material.slug,
-                        },
-                        "size": {
-                            "name": v.size.name,
-                            "sort_order": v.size.sort_order,
-                        },
-                        "material": {
-                            "name": v.material.name,
-                            "slug": v.material.slug,
-                        },
+    "variants": [
+        {
+            "id": v.id,
+            "price": v.variant_price,
+            "stock": v.stock,
+            "is_available": v.is_available,
 
-                        "images": [
-                            {
-                                "id": img.id,
-                                "url": request.host_url.rstrip("/") + "/static/images/" + img.url,
-                                "alt_text": img.alt_text,
-                                "sort_order": img.sort_order,
-                                "variant_id": img.variant_id
-                            } for img in v.images
-                        ]
-                    } for v in p.variants
-                ],
-                "description": p.description,
-                "short_description": p.short_description,
-                "slug": p.slug,
-                "thumbnail": request.host_url.rstrip("/") + "/static/images/" + p.thumbnail,
-                "tags": p.tags,
-                "is_featured": p.is_featured,
-                "created_at": p.created_at,
-                "gender": p.gender.value,
-                "base_price": p.base_price,
-                "category": p.category.name if p.category else None,
-                "brand": p.brand.name if p.brand else None,
-            }
-            for p in products
-        ]
-    }
+            "product": {
+                "id": v.product.id,
+                "name": v.product.name,
+                "slug": v.product.slug,
+                "short_description": v.product.short_description,
+                "description": v.product.description,
+                "thumbnail": request.host_url.rstrip("/") + "/static/images/" + v.product.thumbnail,
+                "tags": v.product.tags,
+                "is_featured": v.product.is_featured,
+                "created_at": v.product.created_at,
+                "gender": v.product.gender.value,
+                "base_price": v.product.base_price,
+                "category": v.product.category.name if v.product.category else None,
+                "brand": v.product.brand.name if v.product.brand else None,
+            },
+
+            "color": {
+                "name": v.color.name,
+                "hex_code": v.color.hex_code,
+                "slug": v.color.slug,
+            },
+
+            "size": {
+                "name": v.size.name,
+                "sort_order": v.size.sort_order,
+            },
+
+            "material": {
+                "name": v.material.name,
+                "slug": v.material.slug,
+            },
+
+            "images": [
+                {
+                    "id": img.id,
+                    "url": request.host_url.rstrip("/") + "/static/images/" + img.url,
+                    "alt_text": img.alt_text,
+                    "sort_order": img.sort_order,
+                    "variant_id": img.variant_id,
+                }
+                for img in v.images
+            ],
+        }
+        for v in variants
+    ]
+}
