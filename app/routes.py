f30051bce58242cff9e8167f15021dec20c893a0
MopuK <stanislavgusev.sv@gmail.com>
2026-07-10 19:17:32 +0300

    Finished moving routes.py to FastAPI

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,21 @@
-from app import app, db, models
+from app import app, models
 from flask import request, abort
 from sqlalchemy import select
-from sqlalchemy.orm import selectinload
-from app.utils import serialize_product, serialize_variant
+from sqlalchemy.orm import selectinload, Session
+from typing import Annotated
+from fastapi import Depends, HTTPException
+from app.schemas import *
+from app.database import get_db
 
+  
 @app.get("/")
 def Home():
     return "Hello"
 
-@app.get("/api/products")
-def getProducts():
+@app.get("/api//v1/products")
+def getProducts(db: Annotated[Session, Depends(get_db)]):
     
-    variants = db.session.scalars(select(models.ProductVariant)
+    variants = db.scalars(select(models.ProductVariant)
                                   .join(models.Product)
                                     .options(
                                         selectinload(models.ProductVariant.product),
@@ -22,91 +26,14 @@ def getProducts():
                                     )).all()
     
     return {
-    "variants": [
-        serialize_variant(v)
-        for v in variants
-    ]
+    "variants": variants
 }
-    
-@app.get("/api/products/<string:productSlug>")
-def getProduct(productSlug):
-    
-    product = db.session.scalar(
-        select(models.Product)
-        .options(
-            selectinload(models.Product.variants)
-                .selectinload(models.ProductVariant.images),
 
-            selectinload(models.Product.variants)
-                .selectinload(models.ProductVariant.color),
+   
+@app.get("/api/v1/products/featured")
+def getFeaturedProducts(db: Annotated[Session, Depends(get_db)]):
 
-            selectinload(models.Product.variants)
-                .selectinload(models.ProductVariant.size),
-
-            selectinload(models.Product.variants)
-                .selectinload(models.ProductVariant.material),
-
-            selectinload(models.Product.category),
-            selectinload(models.Product.brand),
-        )
-        .where(models.Product.slug == productSlug)
-    )
-    
-    if not product:
-        abort(404)
-        
-    return {
-        "product": serialize_product(product)
-    }
-    
-@app.get("/api/categories/<string:category>")
-def getProductsByCategory(category):
-    
-    variants = db.session.scalars(select(models.ProductVariant)
-                                  .join(models.Product)
-                                  .join(models.Category)
-                                    .options(
-                                        selectinload(models.ProductVariant.product),
-                                        selectinload(models.ProductVariant.images),
-                                        selectinload(models.ProductVariant.size),
-                                        selectinload(models.ProductVariant.color),
-                                        selectinload(models.ProductVariant.material),
-                                    )
-                                    .where(models.Category.slug == category)).all()
-    
-    return {
-    "variants": [
-        serialize_variant(v) for v in variants
-    ]
-}
-    
-@app.get("/api/brands/<string:brand>/products")
-def getProductsByBrand(brand):
-
-    variants = db.session.scalars(
-        select(models.ProductVariant)
-        .join(models.Product)
-        .join(models.Brand)
-        .options(
-            selectinload(models.ProductVariant.product),
-            selectinload(models.ProductVariant.images),
-            selectinload(models.ProductVariant.size),
-            selectinload(models.ProductVariant.color),
-            selectinload(models.ProductVariant.material),
-        )
-        .where(models.Brand.slug == brand)
-    ).all()
-
-    return {
-        "variants": [
-        serialize_variant(v) for v in variants
-        ]
-    }
-    
-@app.get("/api/products/featured")
-def getFeaturedProducts():
-
-    products = db.session.scalars(
+    products = db.scalars(
         select(models.Product)
         .options(
             selectinload(models.Product.variants)
@@ -128,8 +55,77 @@ def getFeaturedProducts():
     ).all()
 
     return {
-        "products": [
-            serialize_product(product)
-            for product in products
-        ]
+        "products": products
     }
+    
+@app.get("/api/v1/products/{product_slug}")
+def getProduct(product_slug: str, db: Annotated[Session, Depends(get_db)]):
+    
+    product = db.scalar(
+        select(models.Product)
+        .options(
+            selectinload(models.Product.variants)
+                .selectinload(models.ProductVariant.images),
+
+            selectinload(models.Product.variants)
+                .selectinload(models.ProductVariant.color),
+
+            selectinload(models.Product.variants)
+                .selectinload(models.ProductVariant.size),
+
+            selectinload(models.Product.variants)
+                .selectinload(models.ProductVariant.material),
+
+            selectinload(models.Product.category),
+            selectinload(models.Product.brand),
+        )
+        .where(models.Product.slug == product_slug)
+    )
+    
+    if not product:
+        raise HTTPException(404, f"Product with product slug: {product_slug} not found")
+        
+    return product
+    
+@app.get("/api/v1/categories/{category}")
+def getProductsByCategory(category: str, db: Annotated[Session, Depends(get_db)]):
+    
+    variants = db.scalars(select(models.ProductVariant)
+                                  .join(models.Product)
+                                  .join(models.Category)
+                                    .options(
+                                        selectinload(models.ProductVariant.product),
+                                        selectinload(models.ProductVariant.images),
+                                        selectinload(models.ProductVariant.size),
+                                        selectinload(models.ProductVariant.color),
+                                        selectinload(models.ProductVariant.material),
+                                    )
+                                    .where(models.Category.slug == category)).all()
+    
+    return {
+    "variants": variants,
+    "category": category
+}
+    
+@app.get("/api/v1/brands/{brand}/products")
+def getProductsByBrand(brand: str, db: Annotated[Session, Depends(get_db)]):
+
+    variants = db.scalars(
+        select(models.ProductVariant)
+        .join(models.Product)
+        .join(models.Brand)
+        .options(
+            selectinload(models.ProductVariant.product),
+            selectinload(models.ProductVariant.images),
+            selectinload(models.ProductVariant.size),
+            selectinload(models.ProductVariant.color),
+            selectinload(models.ProductVariant.material),
+        )
+        .where(models.Brand.slug == brand)
+    ).all()
+
+    return {
+        "variants": variants,
+        "brand": brand
+    }
+ 
