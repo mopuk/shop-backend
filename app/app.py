225f6a22f42a2e8c6352864848f2c27b9f8baf77
MopuK <stanislavgusev.sv@gmail.com>
2026-07-14 21:04:40 +0300

    fix: move app to app.py, clear __init__.py

--- app/app.py
+++ app/app.py
@@ -3,28 +3,21 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+
+from app.config import config
 from app.routes import router as api_router
 
 load_dotenv()
 
-origins = [
-    "http://localhost:3000",
-]
-
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
-    allow_headers=[
-        "Content-Type",
-        "Authorization",
-        "Accept",
-        "X-Requested-With",
-        "Idempotency-Key"
-    ],
-    )
+    allow_origins=config.ALLOW_ORIGINS,
+    allow_credentials=config.ALLOW_CREDENTIALS,
+    allow_methods=config.ALLOW_METHODS, 
+    allow_headers=config.ALLOW_HEADERS,
+)
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(api_router)
-
