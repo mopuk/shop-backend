5007e0d224793b3cc8049783e23bfe8d7c3dd4e6
MopuK <stanislavgusev.sv@gmail.com>
2026-08-13 14:28:06 +0300

    feat: add seeding for variants, update references

--- app/seeds/seed.py
+++ app/seeds/seed.py
@@ -2,7 +2,8 @@ import argparse
 import asyncio
 
 from app.seeds.products_seed import reset_products, seed_products
-from app.seeds.reference_seeds import seed_references
+from app.seeds.reference_seeds import reset_references, seed_references
+from app.seeds.variants_seed import reset_variants, seed_variants
 
 
 def parse_args():
@@ -20,12 +21,17 @@ def parse_args():
 
 async def main(reset: bool = False, delete: bool = False):
     if reset:
+        await reset_variants()
         await reset_products()
+        await reset_references()
     if delete:
+        await reset_variants()
         await reset_products()
+        await reset_references()
         return
     await seed_references()
     await seed_products()
+    await seed_variants()
 
 
 if __name__ == "__main__":
