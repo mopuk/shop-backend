20cbb2e9658428e2d7f9e3e398ae4839bfaa3001
MopuK <stanislavgusev.sv@gmail.com>
2026-09-02 12:12:14 +0300

    fix: remove postgres container from docker, minor variable name inconsistency fix in config.py

--- app/config.py
+++ app/config.py
@@ -4,16 +4,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class Config():
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
+
+class Config:
+    DATABASE_URL: str = os.getenv("DATABASE_URI")
 
     JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY")
     JWT_ALGORITHM: str = os.getenv("JWT_ALGORITHM", "HS256")
-    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: str = os.getenv("JWT_ACCESS_TOKEN_EXPIRE_MINUTES", 60*10)
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: str = os.getenv(
+        "JWT_ACCESS_TOKEN_EXPIRE_MINUTES", 60 * 10
+    )
 
-    ALLOW_ORIGINS: list[str] = os.getenv("ALLOW_ORIGINS", "http://localhost:3000,http://127.0.0.1:3000").split(",")
+    ALLOW_ORIGINS: list[str] = os.getenv(
+        "ALLOW_ORIGINS", "http://localhost:3000,http://127.0.0.1:3000"
+    ).split(",")
     ALLOW_CREDENTIALS: bool = True
     ALLOW_METHODS: list[str] = ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-    ALLOW_HEADERS: list[str] = ["Content-Type", "Authorization", "Accept","X-Requested-With","Idempotency-Key"]
+    ALLOW_HEADERS: list[str] = [
+        "Content-Type",
+        "Authorization",
+        "Accept",
+        "X-Requested-With",
+        "Idempotency-Key",
+    ]
+
 
 config = Config()
